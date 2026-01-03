7fb4c16d341a378da13f140a42361e36b9407a28
OldWorldBoa <oldworldboa@gmail.com>
2026-01-03 13:39:11 -0600

    Build website with resources

--- src/build.sh
+++ src/build.sh
@@ -5,9 +5,13 @@ set -e
 js="../elm.js"
 min="../elm.min.js"
 
+# build elm javascript
 elm make --optimize --output=$js "$@"
-
 uglifyjs $js --compress "pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe" | uglifyjs --mangle --output $min
+
+# copy resources
+rm -rf ../res/*
+cp -r ./res/* ../res/
 
 echo "Compiled size:$(wc $js -c) bytes ($js)"
 echo "Compiled size:$(wc $min -c) bytes ($min)"
